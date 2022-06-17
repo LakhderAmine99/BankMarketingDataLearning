2c24762b936338f56d487bef30b7b04e1d7422f6
LakhderAmine99 <amine.lakhder@usmba.ac.ma>
2022-06-17 01:33:46 +0100

    add save and load methods to the serializer module

--- src/module/BankMarketingDataSerializer.py
+++ src/module/BankMarketingDataSerializer.py
@@ -2,8 +2,18 @@ class BankMarketingDataSerializer(object):
     def __init__(self,serializer=None):
         self.serializer = serializer
         
-    def save(self,modelObject=None,name="saved.npy",destination="./model/"):
-        return
+    def save(self,modelObject,filename="model.saved",location=""):
+            
+        dbfile = open(filename,'ab')
+        self.serializer.dump(modelObject,dbfile)
+        dbfile.close()
+        
+        return True
     
-    def load(self,modelObject=None):
-        return
+    def load(self,filename):
+        
+        dbfile = open(filename,'rb')
+        modelObject = self.serializer.load(dbfile)
+        dbfile.close()
+        
+        return modelObject
