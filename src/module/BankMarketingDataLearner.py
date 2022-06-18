431ccec94c7422d0045d9b915b3ff9bc40e69281
LakhderAmine99 <amine.lakhder@usmba.ac.ma>
2022-06-18 02:11:34 +0100

    update Bank Data Learner module and files

--- src/module/BankMarketingDataLearner.py
+++ src/module/BankMarketingDataLearner.py
@@ -1,9 +1,9 @@
-from pyexpat import model
-from sklearn import cluster
+import numpy as np
 from sklearn import metrics
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, recall_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import label_binarize
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 
@@ -25,7 +25,7 @@ class BankMarketingDataLearner(object):
         
     def createKNeighborsModel(self):
         
-        classifier = KNeighborsClassifier(n_neighbors=7,
+        classifier = KNeighborsClassifier(n_neighbors=5,
                                           weights='distance',
                                           algorithm='auto',
                                           leaf_size=30,
@@ -42,7 +42,7 @@ class BankMarketingDataLearner(object):
     
     def createSVMModel(self):
         
-        classifier = SVC(C=1.0,kernel='rbf',gamma='auto',probability=True)
+        classifier = SVC(C=1.0,kernel='rbf',gamma='auto',probability=True,random_state=0)
         
         model = classifier.fit(X=self.X_train,y=self.y_train)
         
@@ -56,15 +56,7 @@ class BankMarketingDataLearner(object):
                                             splitter='best', 
                                             max_depth=None,
                                             min_samples_split=2, 
-                                            min_samples_leaf=1, 
-                                            min_weight_fraction_leaf=0.0, 
-                                            max_features=None, 
-                                            random_state=None, 
-                                            max_leaf_nodes=None, 
-                                            min_impurity_decrease=0.0, 
-                                            min_impurity_split=None, 
-                                            class_weight=None, 
-                                            presort=False)
+                                            min_samples_leaf=1)
         
         model = classifier.fit(X=self.X_train,y=self.y_train)
         
@@ -76,20 +68,10 @@ class BankMarketingDataLearner(object):
        
         classifier = RandomForestClassifier(n_estimators=100,
                                             criterion='gini',
-                                            max_depth=None,
+                                            max_depth=5,
                                             min_samples_split=2, 
                                             min_samples_leaf=1,
-                                            min_weight_fraction_leaf=0.0,
-                                            max_features='auto',
-                                            max_leaf_nodes=None,
-                                            min_impurity_decrease=0.0,
-                                            bootstrap=True,
-                                            oob_score=False,
-                                            n_jobs=1,
-                                            random_state=None,
-                                            verbose=0,
-                                            warm_start=False,
-                                            class_weight=None)
+                                            max_features='auto')
     
         model = classifier.fit(X=self.X_train,y=self.y_train)
         
@@ -111,15 +93,14 @@ class BankMarketingDataLearner(object):
         return
     
     def evaluate(self,estimator=None):
+                
+        _accuracy = accuracy_score(y_true=self.y_test,y_pred=self.predict(estimator=estimator,X=self.X_test))
+        _f1_score = metrics.f1_score(y_true=self.y_test,y_pred=self.predict(estimator=estimator,X=self.X_test),pos_label='yes')
+        _precision = metrics.precision_score(y_true=self.y_test,y_pred=self.predict(estimator=estimator,X=self.X_test),pos_label='yes')
+        _recall = metrics.recall_score(y_true=self.y_test,y_pred=self.predict(estimator=estimator,X=self.X_test),pos_label='yes')
         
-        _accuracy = accuracy_score(y_true=self.y_test,y_pred=estimator.predict(estimator=estimator,X=self.X_test))
-        _f1_score = metrics.f1_score(y_true=self.y_test,y_pred=estimator.predict(estimator=estimator,X=self.X_test))
-        _precision = metrics.precision_score(y_true=self.y_test,y_pred=estimator.predict(estimator=estimator,X=self.X_test))
-        _recall = metrics.recall_score(y_true=self.y_test,y_pred=estimator.predict(estimator=estimator,X=self.X_test))
-        _confusion_matrix = metrics.confusion_matrix(y_true=self.y_test,y_pred=estimator.predict(estimator=estimator,X=self.X_test))
-        _classification_report = metrics.classification_report(y_true=self.y_test,y_pred=estimator.predict(estimator=estimator,X=self.X_test))
         
-        return _accuracy,_f1_score,_precision,_recall,_confusion_matrix,_classification_report
+        return _accuracy,_precision,_f1_score,_recall
     
     def predict(self,estimator=None,X=None):
         return estimator.predict(X)
